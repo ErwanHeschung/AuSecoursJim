355640efad62201c4f7685c77b91b9814381ef94
mathisMAGNIN <mathis.magnin1@etu.univ-cotedazur.fr>
2025-09-15 14:48:37 +0200

    feat: routing menu page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,4 +4,5 @@ import { ROUTES } from './core/utils/constant';
 /*lazy loading*/
 export const routes: Routes = [
     { path: ROUTES.landing, loadComponent: () => import('./features/landing/landing.component').then(m => m.LandingComponent) },
+    { path: ROUTES.menu, loadComponent: () => import('./features/menu/menu.component').then(m => m.MenuComponent) },
 ];

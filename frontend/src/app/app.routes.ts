c9147f8ceb37b313e4f0b89cdb9d4055d92e0f89
Heschung Erwan <erwan.heschung@etu.univ-cotedazur.fr>
2025-09-15 11:42:37 +0200

    feat:add routing template

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ROUTES } from './core/utils/constant';
 
-export const routes: Routes = [];
+/*lazy loading*/
+export const routes: Routes = [
+    { path: ROUTES.landing, loadComponent: () => import('./features/landing/landing.component').then(m => m.LandingComponent) },
+];
